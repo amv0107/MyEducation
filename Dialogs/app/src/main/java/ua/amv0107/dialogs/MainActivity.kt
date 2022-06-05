d53c457d3676fb64d3d70570c0e28de6d9395256
amv0107 <amv0107@gmail.com>
2022-06-06 01:06:47 +0300

    AlertDialog

---
+++ Dialogs/app/src/main/java/ua/amv0107/dialogs/MainActivity.kt
@@ -0,0 +1,27 @@
+package ua.amv0107.dialogs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ua.amv0107.dialogs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.level1Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel1Activity::class.java))
+        }
+
+        binding.level2Button.setOnClickListener {
+            startActivity(Intent(this, DialogsLevel2Activity::class.java))
+        }
+
+        binding.exitButton.setOnClickListener {
+            finish()
+        }
+    }
+}
