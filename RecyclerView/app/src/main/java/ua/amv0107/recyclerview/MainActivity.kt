017bbf3803d705a8ad2350a26e4d06d06ba66121
amv0107 <amv0107@gmail.com>
2022-06-19 19:28:03 +0300

    init RecyclerView

---
+++ RecyclerView/app/src/main/java/ua/amv0107/recyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.amv0107.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
